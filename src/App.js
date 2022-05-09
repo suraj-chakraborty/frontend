44b6c17bdd255402d0d2f79dbd17ef1fcca75fa7
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-05-09 12:19:55 +0000

    redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./Pages/Dashboard";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
@@ -17,6 +19,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import { FaUser } from "react-icons/fa";
+import Spinner from "../components/Spinner";
+import { register, reset} from '../features/auth/authSlice'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,6 +17,26 @@ function Register() {
 
   const { name, email, password, confirmPassword } = formData;
 
+  const navigate = useNavigate()
+
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message} = useSelector( (state) => 
+    state.auth
+  )
+
+  useEffect(() => {
+    if(isError){
+      toast.error(message)
+    }
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -21,7 +46,20 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    if(password !== confirmPassword) {
+      toast.error('Password mismatch')
+    } else {
+      const userData = {
+        name, email, password
+
+      }
+      dispatch(register(userData))
+    }
   };
+
+  if(isLoading) {
+    return <Spinner />
+  }
   return (
     <>
       <section className="header">
@@ -70,8 +108,8 @@ function Register() {
             <input
               type="password"
               className="form-control"
-              id="password"
-              name="password"
+              id="confirmPassword"
+              name="confirmPassword"
               value={confirmPassword}
               placeholder="Enter your Name"
               onChange={onChange}
