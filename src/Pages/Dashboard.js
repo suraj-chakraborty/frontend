5fd6d53dfe7766c6169786090247bc2bf936fc77
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-05-19 21:39:36 +0000

    creating a goal

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import GoalDeshboard from "../components/GoalDeshboard";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <section className="heading">
+      <h1>Welcome {user && user.name}</h1>
+      <GoalDeshboard />
+    </section>
   );
 }
 
